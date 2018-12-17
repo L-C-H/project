8d7a11bfae24f6b1c847a09f2e8be8b790f3ea2c
itzhongzl <42925434+itzhongzl@users.noreply.github.com>
2018-12-17 16:43:15 +0800

    Update add.blade.php

--- resources/views/Admin/Goods/add.blade.php
+++ resources/views/Admin/Goods/add.blade.php
@@ -1,3 +1,5 @@
+2018-12-17 16:41:14
+
 <!--_meta 作为公共模版分离出去-->
 <!DOCTYPE HTML>
 <html>
@@ -24,15 +26,22 @@
 <![endif]-->
 <!--/meta 作为公共模版分离出去-->
 
+
 <title>添加商品</title>
-<meta name="keywords" content="H-ui.admin v3.1,H-ui网站后台模版,后台模版下载,后台管理系统模版,HTML后台模版下载">
-<meta name="description" content="H-ui.admin v3.1，是一款由国人开发的轻量级扁平化网站后台模板，完全免费开源的网站后台管理系统模版，适合中小型CMS后台系统。">
+<style>
+.btn-upload{position: relative; display:inline-block;height:36px; *display:inline;overflow:hidden;vertical-align:middle;cursor:pointer}
+.upload-url{cursor: pointer;}
+.input-file{position:absolute; right:0; top:0; cursor: pointer; z-index:1; font-size:30em; *font-size:30px;opacity:0;filter: alpha(opacity=0)}
+.btn-upload .input-text{ width:300px}
+.form-group .upload-btn{ margin-left:-1px}
+
+
+</style>
 </head>
 <body>
 <article class="page-container">
-	<form class="form form-horizontal" id="form-article-add">
+	<form class="form form-horizontal" id="form-article-add" action="/admingoods" method="post" enctype="multipart/form-data">
 		<div class="row cl">
-<<<<<<< HEAD
 			<label class="form-label col-xs-4 col-sm-2">商品类型：</label>
 			<div class="formControls col-xs-8 col-sm-9">
 				<select class="input-text" name="c_id">
@@ -41,27 +50,21 @@
 				<option value="{{$row->id}}">{{$row->name}}</option>
 				@endforeach
 				</select>
-=======
-			<label class="form-label col-xs-4 col-sm-2"><span class="c-red">*</span>商品名称：</label>
-			<div class="formControls col-xs-8 col-sm-9">
-				<input type="text" class="input-text" value="" placeholder="" id="articletitle" name="articletitle2">
->>>>>>> e633f80d8ba901277a9ac0bb3a7592023373c461
 			</div>
 		</div>
 		<div class="row cl">
-			<label class="form-label col-xs-4 col-sm-2"><span class="c-red">*</span>商品价格：</label>
+			<label class="form-label col-xs-4 col-sm-2">商品名称：</label>
 			<div class="formControls col-xs-8 col-sm-9">
-				<input type="text" class="input-text" value="" placeholder="" id="articletitle" name="articletitle2">
+				<input type="text" class="input-text" value="{{old('goods_name')}}" placeholder="" id="articletitle" name="goods_name">
 			</div>
 		</div>
 		<div class="row cl">
-			<label class="form-label col-xs-4 col-sm-2"><span class="c-red">*</span>商品简描述：</label>
+			<label class="form-label col-xs-4 col-sm-2">商品价格：</label>
 			<div class="formControls col-xs-8 col-sm-9">
-				<input type="text" class="input-text" value="" placeholder="" id="articletitle" name="articletitle2">
+				<input type="text" class="input-text" value="{{old('goods_price')}}" placeholder="" id="articletitle" name="goods_price">
 			</div>
 		</div>
 		<div class="row cl">
-<<<<<<< HEAD
 			<label class="form-label col-xs-4 col-sm-2">商品原价格：</label>
 			<div class="formControls col-xs-8 col-sm-9">
 				<input type="text" class="input-text" value="{{old('original_price')}}" placeholder="" id="articletitle" name="original_price">
@@ -69,15 +72,11 @@
 		</div>
 		<div class="row cl">
 			<label class="form-label col-xs-4 col-sm-2">商品库存：</label>
-=======
-			<label class="form-label col-xs-4 col-sm-2"><span class="c-red">*</span>品牌名称：</label>
->>>>>>> e633f80d8ba901277a9ac0bb3a7592023373c461
 			<div class="formControls col-xs-8 col-sm-9">
-				<input type="text" class="input-text" value="" placeholder="" id="articletitle" name="articletitle2">
+				<input type="text" class="input-text" value="{{old('stock')}}" placeholder="" id="articletitle" name="stock">
 			</div>
 		</div>
 		<div class="row cl">
-<<<<<<< HEAD
 			<label class="form-label col-xs-4 col-sm-2">商品颜色：</label>
 			<div class="formControls col-xs-8 col-sm-9">
 				<input type="text" class="input-text" value="{{old('color')}}" placeholder="" id="articletitle" name="color">
@@ -111,227 +110,4 @@
             <label class="form-label col-xs-4 col-sm-2">商品图片：</label>
             <div class="formControls col-xs-8 col-sm-9">
             <span class="btn-upload form-group">
-  				<input class="input-text upload-url radius" type="text" name="pic" id="uploadfile-1" readonly value="{{old('pic')}}"><a href="javascript:void();" class="btn btn-primary radius"><i class="iconfont">&#xf0020;</i> 选择文件</a>
-  				<input type="file" multiple name="pic" class="input-file"></span>
-            </div>
-        </div>
-=======
-			<label class="form-label col-xs-4 col-sm-2"><span class="c-red">*</span>商品库存：</label>
-			<div class="formControls col-xs-8 col-sm-9">
-				<input type="text" class="input-text" value="" placeholder="" id="articletitle" name="articletitle2">
-			</div>
-		</div>
->>>>>>> e633f80d8ba901277a9ac0bb3a7592023373c461
-		<div class="row cl">
-			<label class="form-label col-xs-4 col-sm-2">商品图片：</label>
-			<div class="formControls col-xs-8 col-sm-9">
-				<div class="uploader-thum-container">
-					<div id="fileList" class="uploader-list"></div>
-					<div id="filePicker">选择图片</div>
-					<button id="btn-star" class="btn btn-default btn-uploadstar radius ml-10">开始上传</button>
-				</div>
-			</div>
-		</div>
-		<div class="row cl">
-			<label class="form-label col-xs-4 col-sm-2">具体描述：</label>
-			<div class="formControls col-xs-8 col-sm-9"> 
-				<script id="editor" type="text/plain" style="width:100%;height:400px;"></script> 
-			</div>
-		</div>
-		<div class="row cl">
-			<div class="col-xs-8 col-sm-9 col-xs-offset-4 col-sm-offset-2">
-<<<<<<< HEAD
-			{{csrf_field()}}
-				<button class="btn btn-primary radius" type="submit"><i class="Hui-iconfont">&#xe632;</i> 保存并提交审核</button>
-				<a class="btn btn-default radius" type="button" href="/admingoods">&nbsp;&nbsp;取消&nbsp;&nbsp;</a>
-=======
-				<button onClick="article_save_submit();" class="btn btn-primary radius" type="submit"><i class="Hui-iconfont">&#xe632;</i> 保存并提交审核</button>
-				<button onClick="article_save();" class="btn btn-secondary radius" type="button"><i class="Hui-iconfont">&#xe632;</i> 保存草稿</button>
-				<button onClick="removeIframe();" class="btn btn-default radius" type="button">&nbsp;&nbsp;取消&nbsp;&nbsp;</button>
->>>>>>> e633f80d8ba901277a9ac0bb3a7592023373c461
-			</div>
-		</div>
-	</form>
-</article>
-
-<!--_footer 作为公共模版分离出去-->
-<script type="text/javascript" src="/static/lib/jquery/1.9.1/jquery.min.js"></script> 
-<script type="text/javascript" src="/static/lib/layer/2.4/layer.js"></script>
-<script type="text/javascript" src="/static/static/h-ui/js/H-ui.min.js"></script> 
-<script type="text/javascript" src="/static/static/h-ui.admin/js/H-ui.admin.js"></script> <!--/_footer /作为公共模版分离出去-->
-
-<!--请在下方写此页面业务相关的脚本-->
-<script type="text/javascript" src="/static/lib/My97DatePicker/4.8/WdatePicker.js"></script>
-<script type="text/javascript" src="/static/lib/jquery.validation/1.14.0/jquery.validate.js"></script> 
-<script type="text/javascript" src="/static/lib/jquery.validation/1.14.0/validate-methods.js"></script> 
-<script type="text/javascript" src="/static/lib/jquery.validation/1.14.0/messages_zh.js"></script>
-<script type="text/javascript" src="/static/lib/webuploader/0.1.5/webuploader.min.js"></script> 
-<script type="text/javascript" src="/static/lib/ueditor/1.4.3/ueditor.config.js"></script> 
-<script type="text/javascript" src="/static/lib/ueditor/1.4.3/ueditor.all.min.js"> </script> 
-<script type="text/javascript" src="/static/lib/ueditor/1.4.3/lang/zh-cn/zh-cn.js"></script>
-<script type="text/javascript">
-$(function(){
-	$('.skin-minimal input').iCheck({
-		checkboxClass: 'icheckbox-blue',
-		radioClass: 'iradio-blue',
-		increaseArea: '20%'
-	});
-	
-	//表单验证
-	$("#form-article-add").validate({
-		rules:{
-			articletitle:{
-				required:true,
-			},
-			articletitle2:{
-				required:true,
-			},
-			articlecolumn:{
-				required:true,
-			},
-			articletype:{
-				required:true,
-			},
-			articlesort:{
-				required:true,
-			},
-			keywords:{
-				required:true,
-			},
-			abstract:{
-				required:true,
-			},
-			author:{
-				required:true,
-			},
-			sources:{
-				required:true,
-			},
-			allowcomments:{
-				required:true,
-			},
-			commentdatemin:{
-				required:true,
-			},
-			commentdatemax:{
-				required:true,
-			},
-
-		},
-		onkeyup:false,
-		focusCleanup:true,
-		success:"valid",
-		submitHandler:function(form){
-			//$(form).ajaxSubmit();
-			var index = parent.layer.getFrameIndex(window.name);
-			//parent.$('.btn-refresh').click();
-			parent.layer.close(index);
-		}
-	});
-	
-	$list = $("#fileList"),
-	$btn = $("#btn-star"),
-	state = "pending",
-	uploader;
-
-	var uploader = WebUploader.create({
-		auto: true,
-		swf: 'lib/webuploader/0.1.5/Uploader.swf',
-	
-		// 文件接收服务端。
-		server: 'fileupload.php',
-	
-		// 选择文件的按钮。可选。
-		// 内部根据当前运行是创建，可能是input元素，也可能是flash.
-		pick: '#filePicker',
-	
-		// 不压缩image, 默认如果是jpeg，文件上传前会压缩一把再上传！
-		resize: false,
-		// 只允许选择图片文件。
-		accept: {
-			title: 'Images',
-			extensions: 'gif,jpg,jpeg,bmp,png',
-			mimeTypes: 'image/*'
-		}
-	});
-	uploader.on( 'fileQueued', function( file ) {
-		var $li = $(
-			'<div id="' + file.id + '" class="item">' +
-				'<div class="pic-box"><img></div>'+
-				'<div class="info">' + file.name + '</div>' +
-				'<p class="state">等待上传...</p>'+
-			'</div>'
-		),
-		$img = $li.find('img');
-		$list.append( $li );
-	
-		// 创建缩略图
-		// 如果为非图片文件，可以不用调用此方法。
-		// thumbnailWidth x thumbnailHeight 为 100 x 100
-		uploader.makeThumb( file, function( error, src ) {
-			if ( error ) {
-				$img.replaceWith('<span>不能预览</span>');
-				return;
-			}
-	
-			$img.attr( 'src', src );
-		}, thumbnailWidth, thumbnailHeight );
-	});
-	// 文件上传过程中创建进度条实时显示。
-	uploader.on( 'uploadProgress', function( file, percentage ) {
-		var $li = $( '#'+file.id ),
-			$percent = $li.find('.progress-box .sr-only');
-	
-		// 避免重复创建
-		if ( !$percent.length ) {
-			$percent = $('<div class="progress-box"><span class="progress-bar radius"><span class="sr-only" style="width:0%"></span></span></div>').appendTo( $li ).find('.sr-only');
-		}
-		$li.find(".state").text("上传中");
-		$percent.css( 'width', percentage * 100 + '%' );
-	});
-	
-	// 文件上传成功，给item添加成功class, 用样式标记上传成功。
-	uploader.on( 'uploadSuccess', function( file ) {
-		$( '#'+file.id ).addClass('upload-state-success').find(".state").text("已上传");
-	});
-	
-	// 文件上传失败，显示上传出错。
-	uploader.on( 'uploadError', function( file ) {
-		$( '#'+file.id ).addClass('upload-state-error').find(".state").text("上传出错");
-	});
-	
-	// 完成上传完了，成功或者失败，先删除进度条。
-	uploader.on( 'uploadComplete', function( file ) {
-		$( '#'+file.id ).find('.progress-box').fadeOut();
-	});
-	uploader.on('all', function (type) {
-        if (type === 'startUpload') {
-            state = 'uploading';
-        } else if (type === 'stopUpload') {
-            state = 'paused';
-        } else if (type === 'uploadFinished') {
-            state = 'done';
-        }
-
-        if (state === 'uploading') {
-            $btn.text('暂停上传');
-        } else {
-            $btn.text('开始上传');
-        }
-    });
-
-    $btn.on('click', function () {
-        if (state === 'uploading') {
-            uploader.stop();
-        } else {
-            uploader.upload();
-        }
-    });
-	
-	var ue = UE.getEditor('editor');
-	
-});
-</script>
-<!--/请在上方写此页面业务相关的脚本-->
-</body>
-</html>
+  				<input class="input-text upload-url radius" type="text" name="pic" id="uploadfile-1" readonly value="{{old('pic')}}"><a href="jav
