1eee29fbbaa1340a4dfe1c283f7c62f2438e1331
itzhongzl <42925434+itzhongzl@users.noreply.github.com>
2018-12-17 16:44:50 +0800

    Update index.blade.php

--- resources/views/Admin/Cates/index.blade.php
+++ resources/views/Admin/Cates/index.blade.php
@@ -23,16 +23,20 @@
 </head>
 <body>
 <nav class="breadcrumb"><i class="Hui-iconfont">&#xe67f;</i> 首页 <span class="c-gray en">&gt;</span> 商品管理 <span class="c-gray en">&gt;</span> 分类列表 <a class="btn btn-success radius r" style="line-height:1.6em;margin-top:3px" href="javascript:location.replace(location.href);" title="刷新" ><i class="Hui-iconfont">&#xe68f;</i></a></nav>
+	@if(session('success'))
+		<div class="Huialert Huialert-success"><i class="Hui-iconfont">&#xe6a6;</i>{{session('success')}}</div>
+	@endif
+	@if(session('error'))
+		<div class="Huialert Huialert-danger"><i class="Hui-iconfont">&#xe6a6;</i>{{session('error')}}</div>
+	@endif
 <div class="page-container">
-	<div class="text-c">
-	日期范围：
-		<input type="text" onfocus="WdatePicker({ maxDate:'#F{$dp.$D(\'logmax\')||\'%y-%M-%d\'}' })" id="logmin" class="input-text Wdate" style="width:120px;">
-		-
-		<input type="text" onfocus="WdatePicker({ minDate:'#F{$dp.$D(\'logmin\')}',maxDate:'%y-%M-%d' })" id="logmax" class="input-text Wdate" style="width:120px;">
-		<input type="text" name="" id="" placeholder=" 分类名称" style="width:250px" class="input-text">
-		<button name="" id="" class="btn btn-success" type="submit"><i class="Hui-iconfont">&#xe665;</i> 搜分类名称</button>
-	</div>
-	<div class="cl pd-5 bg-1 bk-gray mt-20"> <span class="l"><a href="javascript:;" onclick="datadel()" class="btn btn-danger radius"><i class="Hui-iconfont">&#xe6e2;</i> 批量删除</a> <a class="btn btn-primary radius" data-title="添加品牌" data-href="/admincates/create" onclick="Hui_admin_tab(this)" href="javascript:;"><i class="Hui-iconfont">&#xe600;</i> 添加分类</a></span> <span class="r">共有数据：<strong>54</strong> 条</span> </div>
+	<form action="/admincates" method="get">
+		<div class="text-c">
+			<input type="text" name="keywords" id="" placeholder=" 分类名称" style="width:250px" class="input-text" value="{{$request['keywords'] or ''}}">
+			<button name="" id="" class="btn btn-success" type="submit"><i class="Hui-iconfont">&#xe665;</i> 搜分类名称</button>
+		</div>
+	</form>
+	<div class="cl pd-5 bg-1 bk-gray mt-20"> <span class="l"><a href="javascript:;" onclick="datadel()" class="btn btn-danger radius"><i class="Hui-iconfont">&#xe6e2;</i> 批量删除</a> <a class="btn btn-primary radius" data-title="添加品牌" data-href="/admincates/create" onclick="Hui_admin_tab(this)" href="javascript:;"><i class="Hui-iconfont">&#xe600;</i> 添加分类</a></span> <span class="r">共有数据：<strong>{{count($cate)}}</strong> 条</span> </div>
 	<div class="mt-20">
 		<table class="table table-border table-bordered table-bg table-sort">
 			<thead>
@@ -42,21 +46,35 @@
 					<th width="80">分类名称</th>
 					<th width="70">pid</th>
 					<th width="120">path</th>
-
+					<th width="25">是否上架</th>
 					<th width="100">操作</th>
 				</tr>
 			</thead>
 			<tbody>
+				@foreach($cate as $row)
 				<tr class="text-c">
 					<td><input name="" type="checkbox" value=""></td>
-					<td>1</td>
-					<td>一级分类</td>
-					<td>0</td>
-          <td>xxx</td>
-					<td class="f-14 td-manage"><a style="text-decoration:none" onClick="article_stop(this,'10001')" href="javascript:;" title="下架"><i class="Hui-iconfont">&#xe6de;</i></a> <a style="text-decoration:none" class="ml-5" onClick="article_edit('分类编辑','/admincates/1/edit','10001')" href="javascript:;" title="编辑"><i class="Hui-iconfont">&#xe6df;</i></a> <a style="text-decoration:none" class="ml-5" onClick="article_del(this,'10001')" href="javascript:;" title="删除"><i class="Hui-iconfont">&#xe6e2;</i></a></td>
-				</tr>
+					<td class="id">{{$row->id}}</td>		
+					<td>{{$row->name}}</td>
+					<td>{{$row->pid}}</td>
+          			<td>{{$row->path}}</td>
+          			<td><span class="label label-success radius">{{$row->display}}</span></td>
+					<td class="f-14 td-manage">
+						<a style="text-decoration:none" class="dis" href="javascript:;" title="下架"><i class="Hui-iconfont">{{$row->display=='下架'?'&#xe6de;':'&#xe6dc;'}}</i></a>
+						<!-- onClick="article_stop(this,{{$row->id}})" -->
+						<a style="text-decoration:none" class="ml-5" href="/admincates/{{$row->id}}/edit" title="编辑"><i class="Hui-iconfont">&#xe6df;</i></a>
+						<!-- <form action="/admincates/{{$row->id}}" method="post">
+							{{csrf_field()}}
+							{{ method_field('DELETE') }}
+							<button type="submit" style="text-decoration:none" class="ml-5" title="删除"><i class="Hui-iconfont">&#xe6e2;</i></button>
+						</form> -->
+						<a href="javascript:;" style="text-decoration:none" class="ml-5 del" title="删除"><i class="Hui-iconfont">&#xe6e2;</i></a>
+					</td>
+				</tr>	
+				@endforeach
 			</tbody>
 		</table>
+		{{$cate->appends($request)->render()}}
 	</div>
 </div>
 <!--_footer 作为公共模版分离出去-->
@@ -70,89 +88,52 @@
 <script type="text/javascript" src="/static/lib/My97DatePicker/4.8/WdatePicker.js"></script> 
 <script type="text/javascript" src="/static/lib/datatables/1.10.0/jquery.dataTables.min.js"></script> 
 <script type="text/javascript" src="/static/lib/laypage/1.2/laypage.js"></script>
-<script type="text/javascript">
+<script>
+	// ajax删除
+	// alert($);
+	$('.del').click(function(){
+		id=$(this).parents('tr').find('td').eq(1).html();
+		s=$(this).parents('tr');
+		ss=confirm('确定要删除吗?');
+		$.get('/admincatessss',{'id':id},function(data){
+			// console.log(data);
+			if(ss){
+				if (data==1) {
+					alert('删除成功');
+					s.remove();
+				}else{
+					alert('请先删除子类');
+				}	
+			}			
+		});
+	});
 
-/*资讯-添加*/
-function article_add(title,url,w,h){
-	var index = layer.open({
-		type: 2,
-		title: title,
-		content: url
+//ajax下架发布
+$('.dis').click(function(){
+	id=$(this).parents('tr').find('.id').html();
+	// alert(id);
+	s=$(this);
+	$.get('/admincatesdis',{'id':id},function(data){
+	// alert(data);
+		if(data==1){
+			//1表示下架
+			//修改为禁用状态
+			s.parents('td').prev().find('span').html('下架');
+			//修改图标
+			s.find('i').html('&#xe6de;');
+			//弹窗提示已下架
+			layer.msg('下架成功!',{icon: 5,time:1000});
+		}else if(data==0){
+			//0表示上架
+			//修改为上架状态
+			s.parents('td').prev().find('span').html('上架');
+			// 修改图标
+			s.find('i').html('&#xe6dc;');
+			//弹窗提示发布成功
+			layer.msg('发布成功!',{icon: 6,time:1000});
+		}
 	});
-	layer.full(index);
-}
-/*资讯-编辑*/
-function article_edit(title,url,id,w,h){
-	var index = layer.open({
-		type: 2,
-		title: title,
-		content: url
-	});
-	layer.full(index);
-}
-/*资讯-删除*/
-function article_del(obj,id){
-	layer.confirm('确认要删除吗？',function(index){
-		$.ajax({
-			type: 'POST',
-			url: '',
-			dataType: 'json',
-			success: function(data){
-				$(obj).parents("tr").remove();
-				layer.msg('已删除!',{icon:1,time:1000});
-			},
-			error:function(data) {
-				console.log(data.msg);
-			},
-		});		
-	});
-}
-
-/*资讯-审核*/
-function article_shenhe(obj,id){
-	layer.confirm('审核文章？', {
-		btn: ['通过','不通过','取消'], 
-		shade: false,
-		closeBtn: 0
-	},
-	function(){
-		$(obj).parents("tr").find(".td-manage").prepend('<a class="c-primary" onClick="article_start(this,id)" href="javascript:;" title="申请上线">申请上线</a>');
-		$(obj).parents("tr").find(".td-status").html('<span class="label label-success radius">已发布</span>');
-		$(obj).remove();
-		layer.msg('已发布', {icon:6,time:1000});
-	},
-	function(){
-		$(obj).parents("tr").find(".td-manage").prepend('<a class="c-primary" onClick="article_shenqing(this,id)" href="javascript:;" title="申请上线">申请上线</a>');
-		$(obj).parents("tr").find(".td-status").html('<span class="label label-danger radius">未通过</span>');
-		$(obj).remove();
-    	layer.msg('未通过', {icon:5,time:1000});
-	});	
-}
-/*资讯-下架*/
-function article_stop(obj,id){
-	layer.confirm('确认要下架吗？',function(index){
-		$(obj).parents("tr").find(".td-manage").prepend('<a style="text-decoration:none" onClick="article_start(this,id)" href="javascript:;" title="发布"><i class="Hui-iconfont">&#xe603;</i></a>');
-		$(obj).parents("tr").find(".td-status").html('<span class="label label-defaunt radius">已下架</span>');
-		$(obj).remove();
-		layer.msg('已下架!',{icon: 5,time:1000});
-	});
-}
-
-/*资讯-发布*/
-function article_start(obj,id){
-	layer.confirm('确认要发布吗？',function(index){
-		$(obj).parents("tr").find(".td-manage").prepend('<a style="text-decoration:none" onClick="article_stop(this,id)" href="javascript:;" title="下架"><i class="Hui-iconfont">&#xe6de;</i></a>');
-		$(obj).parents("tr").find(".td-status").html('<span class="label label-success radius">已发布</span>');
-		$(obj).remove();
-		layer.msg('已发布!',{icon: 6,time:1000});
-	});
-}
-/*资讯-申请上线*/
-function article_shenqing(obj,id){
-	$(obj).parents("tr").find(".td-status").html('<span class="label label-default radius">待审核</span>');
-	$(obj).parents("tr").find(".td-manage").html("");
-	layer.msg('已提交申请，耐心等待审核!', {icon: 1,time:2000});
-}
+});
 
 </script> 
 </body>
